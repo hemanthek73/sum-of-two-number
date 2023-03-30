71b9125c04e905316c010d36faa5ef5b45566de9
HEMANTH KUMAR  EK <125449369+hemanthek73@users.noreply.github.com>
2023-03-30 20:05:34 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,11 @@
+class sum{
+    public static void main(String[] args){
+        int a=10;
+        int b=15;
+        int sum=a+b;
+        System.out.println("the sum of two number:"+sum);
+    }
+}
+
+    
+
